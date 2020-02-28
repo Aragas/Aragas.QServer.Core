fbfff9af486f195212ac92e6882be4092ecda531
Vitaly Mikhailov <personal@aragas.org>
2020-02-29 00:23:17 +0300

    Init

---
+++ Extensions/PacketTransmissionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Aragas.Network.Packets;
+
+namespace Aragas.Network.IO
+{
+    public static class PacketTransmissionExtension
+    {
+        public static bool TryReadPacket<TPacket, TIDType>(this PacketTransmission<TPacket, TIDType> transmission, out TPacket? packet)
+            where TPacket : Packet<TIDType>
+            => (packet = transmission.ReadPacket()) != null;
+    }
+}
---
+++ Protocol/ProtobufNetworkBusTransmission.cs
@@ -0,0 +1,15 @@
+﻿using Aragas.Network.Data;
+using Aragas.Network.IO;
+using Aragas.Network.Packets;
+using Aragas.QServer.NetworkBus;
+
+using System;
+
+namespace Aragas.QServer.Core.Protocol
+{
+    public abstract class ProtobufNetworkBusTransmission<TProtobufPacketType> : SocketPacketNetworkBusTransmission<TProtobufPacketType, VarInt, ProtobufSerializer, ProtobufDeserializer>
+        where TProtobufPacketType : Packet<VarInt>
+    {
+        protected ProtobufNetworkBusTransmission(IAsyncNetworkBus networkBus, Guid playerId, BasePacketFactory<TProtobufPacketType, VarInt>? factory = null) : base(networkBus, playerId, factory) { }
+    }
+}
